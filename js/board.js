8da26c66a07b89afcb751bdfcf66125759e66432
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-03-13 10:07:20 +0200

    Add random items on board

--- js/board.js
+++ js/board.js
@@ -17,3 +17,31 @@ const Map = function(mapSize) {
 // create game map object
 let game = new Map(mapSize);
 
+// randomize the boxes on the map to randomize position of game's components
+function random(num) {
+    return Math.floor(Math.random() * num);
+}
+
+/* add components to the map function like obstacles, weapon, players, 
+which is used by 'add' function by their function constructor */
+function addComponents(itemClass, player) {
+    let restOfTiles = tiles;
+    let boxes = $('.box');
+    let empty = true;
+    while (empty) {
+        let item = random(mapSize);
+        if (player === 1) {
+            positionRules = (item % 10 === 0);
+        } else if (player === 2) {
+            positionRules = (item % 10 === 9);
+        } else {
+            positionRules = (item % 10 !== 0 && item % 10 !== 9);
+        }
+        if (positionRules && restOfTiles.includes(item)) {
+            boxes.eq(item).addClass(itemClass);
+            let index = restOfTiles.indexOf(item);
+            restOfTiles.splice(index, 1);
+            empty = false;
+        }
+    }
+}
