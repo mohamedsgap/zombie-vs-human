ec1a3d1fde72eca6e7b996b5f054c384bf3bef8d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-03-13 10:50:24 +0200

    Create four weapons

--- js/weapons.js
+++ js/weapons.js
@@ -11,3 +11,9 @@ function Weapon(type, value, itemClass) {
 };
 
 
+// create weapons for the game
+let firstWeapon = new Weapon("firstWeapon", 15, "wp-1 weapon");
+let secondWeapon = new Weapon("secondWeapon", 30, "wp-2 weapon");
+let thirdWeapon = new Weapon("thirdWeapon", 45, "wp-3 weapon");
+let fourthWeapon = new Weapon("fourthWeapon", 60, "wp-4 weapon");
+
